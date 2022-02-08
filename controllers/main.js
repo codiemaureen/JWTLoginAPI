a3117ef0c5993e00fd5e6229b2518e42cd37a516
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-08 12:52:02 -0500

    generating token

--- controllers/main.js
+++ controllers/main.js
@@ -1,8 +1,21 @@
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error');
+
 
 const login = async(req, res) => {
     const {username, password} = req.body;
     console.log(username, password);
-    res.send('Fake Login/Register/Signup Route');
+
+    if(!username || !password){
+        throw new CustomAPIError('Please provide email and password', 400);
+    };
+
+    const id = new Date().getDate();
+
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET,{expiresIn:'30d'});;
+
+    res.status(200).json({msg:`User Created`, token});
+
 };
 
 const dashboard = async (req, res) => {
